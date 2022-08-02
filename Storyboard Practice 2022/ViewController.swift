24e4e42e1451d42c95996efd7de5340cf02f2f22
Amara Halley <amarajhalley@icloud.com>
2022-08-02 13:33:02 -0400

    Initial Commit

---
+++ Storyboard Practice 2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storyboard Practice 2022
+//
+//  Created by Amara Halley on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
